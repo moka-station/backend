dae068256a3cd2fac8b47efc94eebfdade6ad123
moka-ayumu <dev.ayumu@gmail.com>
2023-02-20 00:41:10 +0900

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,8 @@ import Config
 
 config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION")
 
 config :backend,
   username: System.get_env("USERNAME"),
